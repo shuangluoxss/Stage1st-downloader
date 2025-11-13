d523440a5c299a4e53fd40c18391206d282bf142
宋旭东 <songxudong@goldwind.com>
2025-11-13 08:23:06 +0800

    更新S1域名

--- S1_downloader.py
+++ S1_downloader.py
@@ -1,5 +1,5 @@
 import time
-from PyQt5.QtCore import (QThread, pyqtSignal)
+from PyQt5.QtCore import QThread, pyqtSignal
 from downloader import S1Downloader, login
 
 
@@ -8,7 +8,7 @@ class WorkThread(QThread):
 
     def __init__(self):
         super(WorkThread, self).__init__()
-        self.url = ''
+        self.url = ""
         # self.sess = requests.Session()
         self.downloader: S1Downloader = S1Downloader()
         self.downloader.on_exception = self.on_exception
@@ -19,34 +19,36 @@ class WorkThread(QThread):
         self.downloader.on_process = self.on_process
 
     def on_exception(self, e):
-        self.trigger.emit(['error', e])
+        self.trigger.emit(["error", e])
 
     def on_error(self, msg):
-        self.trigger.emit(['error', msg])
+        self.trigger.emit(["error", msg])
 
     def on_update(self, current_page, total_pages, title):
-        self.trigger.emit(['refresh', current_page, total_pages, title])
+        self.trigger.emit(["refresh", current_page, total_pages, title])
 
     def on_start(self):
-        self.trigger.emit(['start'])
+        self.trigger.emit(["start"])
 
     def on_finish(self, total_pages, title):
-        self.trigger.emit(['finished', total_pages, title])
+        self.trigger.emit(["finished", total_pages, title])
 
     def on_process(self, source):
-        self.trigger.emit(['process', source])
+        self.trigger.emit(["process", source])
 
     def run(self):
         try:
             self.downloader.download_thread(self.url)
         except Exception as e:
-            self.trigger.emit(['error', e])
+            self.trigger.emit(["error", e])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     wt = WorkThread()
-    wt.url = 'https://bbs.saraba1st.com/2b/thread-1911768-1-1.html'
+    wt.url = "https://stage1st.com/2b/thread-1911768-1-1.html"
     while True:
-        total_page = wt.download_thread('https://bbs.saraba1st.com/2b/thread-1911768-1-1.html', force_refresh=True)
+        total_page = wt.download_thread(
+            "https://stage1st.com/2b/thread-1911768-1-1.html", force_refresh=True
+        )
         print(time.strftime("%m-%d %H:%M:%S", time.localtime()), total_page)
         time.sleep(600)
